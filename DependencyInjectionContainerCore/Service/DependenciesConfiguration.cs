611da0de2259ed7b7b2686a88cbd7c65dd120991
Anton <antondanik32@mail.ru>
2022-12-10 13:17:08 +0300

    Create service for configure the container.

---
+++ DependencyInjectionContainerCore/Service/DependenciesConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using DependencyInjectionContainerCore.Model;
+
+namespace DependencyInjectionContainerCore.Service
+{
+    public class DependenciesConfiguration
+    {
+        public readonly Dictionary<Type, List<ImplementationInfo>> RegisteredDependencies;
+
+        public DependenciesConfiguration()
+        {
+            RegisteredDependencies = new Dictionary<Type, List<ImplementationInfo>>();
+        }
+
+        public void Register<TDependency, TImplementation>(LifeTime lt = LifeTime.InstancePerDependency)
+        {
+            Register(typeof(TDependency), typeof(TImplementation), lt);
+        }
+
+        public void Register(Type interfaceType, Type classType, LifeTime lt = LifeTime.InstancePerDependency)
+        {
+            if ((!interfaceType.IsInterface && interfaceType != classType) || classType.IsAbstract || !interfaceType.IsAssignableFrom(classType) && !interfaceType.IsGenericTypeDefinition)
+                throw new Exception("Registration exception");
+            if (!RegisteredDependencies.ContainsKey(interfaceType))
+            {
+                List<ImplementationInfo> impl = new List<ImplementationInfo> {new ImplementationInfo(lt, classType)};
+                RegisteredDependencies.Add(interfaceType, impl);
+            }
+            else
+            {
+                RegisteredDependencies[interfaceType].Add(new ImplementationInfo(lt, classType));
+            }
+        }
+    }
+}
